44c12f25240869e04ae8dbcc778189eb6606b631
Vilgeir <vilgeir@gmail.com>
2022-03-02 14:21:29 +0100

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCatalougePage } from './pages/pokemon-catalouge/pokemon-catalouge.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
+//Decorator
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    PokemonCatalougePage,
+    TrainerPage
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/pokemon-catalouge/pokemon-catalouge.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-catalouge',
+  templateUrl: './pokemon-catalouge.page.html',
+  styleUrls: ['./pokemon-catalouge.page.css']
+})
+export class PokemonCatalougePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
