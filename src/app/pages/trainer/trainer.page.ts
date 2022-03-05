2b9ca86ca9b143fe7213a6260e55410edf09ce59
Anne E. Larsen <anneelarsen98@gmail.com>
2022-03-05 18:01:53 +0100

    thinking

--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { PokemonCatalogueService } from 'src/app/services/pokemon-catalogue.service';
+
 
 @Component({
   selector: 'app-trainer',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerPage implements OnInit {
 
-  constructor() { }
+  get pokemon(): Pokemon[] {
+    return this.pokemonCatalogueService.pokemon;
+  }
+  get error(): string {
+    return this.pokemonCatalogueService.error;
+  }
+  get loading(): boolean {
+    return this.pokemonCatalogueService.loading;
+  }
+
+  // same as pokemon catalouge page atm, DO change
+  // Should read from trainer api and get list of favourite pokemon.
+  // If they are stored with IDs, can display same as catalouge page.
+  // Workaround, can display pictures based on names(?? endpoint, other URL)
+  // Write separate functions for this, trainer service elns
+  constructor(private readonly pokemonCatalogueService: PokemonCatalogueService
+    ) { }
 
   ngOnInit(): void {
+    this.pokemonCatalogueService.findPokemon();
   }
 
 }
